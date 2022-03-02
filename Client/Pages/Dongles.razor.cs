2521004e72b50d5b73f2cd74243193c230302dc0
anton.kharchenko <kirilsambros@gmail.com>
2022-03-02 18:15:39 +0200

    Added Localization for Blazor project

---
+++ Client/Pages/Dongles.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace Client.Pages;
+
+public partial class Dongles
+{
+    bool isOpen;
+    private string serialNumber = "";
+    
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+    }
+}
--- CloudCustomers.Clients.Tests/Components/DonglePageTests.cs
+++ CloudCustomers.Clients.Tests/Components/DonglePageTests.cs
@@ -1,10 +1,24 @@
-﻿namespace CloudCustomers.Clients.Tests.Components;
+﻿using AngleSharp.Dom;
+using Bunit;
+using Client.Pages;
+
+namespace CloudCustomers.Clients.Tests.Components;
 
 public class DonglePageTests
 {
     [Fact]
     public void InformationShouldViewWhenSelectedDongle()
     {
+        // Arrange
+        using var context = new TestContext();
+        var renderComponent = context.RenderComponent<Dongles>();
+        var element = renderComponent.Find("mud-expand-panel mud-panel-expanded mud-elevation-1 mud-expand-panel-border");
         
+        // Act
+        renderComponent.Find("button").Click();
+        var ListOfSerialNumbers = element.AwaitEventAsync("").Result;
+        
+        // Assert
+        ListOfSerialNumbers;
     }
 }
